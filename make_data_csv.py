7182e40de740d6b786e870eb6b381467683e8621
PrakharMohanAGarwal <prakharmohan699@gmail.com>
2019-04-03 14:39:16 -0500

    Script Modified: in to the function

--- make_data_csv.py
+++ make_data_csv.py
@@ -5,43 +5,47 @@ import csv
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--num_user', help='number of users to extract', type=int, default=2000)
-parser.add_argument('--num_item', help='number of movies to items', type=int, default=2000)
-parser.add_argument('--dir', help='target directory for csv', default="./data")
-args = parser.parse_args()
+def create_subdata(num_user = 2000, num_item = 2000, path = "./data"):
+    try:
+        if not os.path.exists(path):
+            os.mkdir(path)
+    except OSError as er:
+        print ("Error with directory ", path, ". Error message: ", err)
+        path = "./"
+    
+    with open('./goodbooks-10k/ratings.csv') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        data = []  # list(csv_reader)
+        head = 0
+        for row in csv_reader:
+            # print(data)
+            if head == 0:
+                head = 1
+                continue
+    
+            if int(row[0]) <= num_users and int(row[1]) <= num_items:
+                data.append(row)
+                # print(row)
+    
+    filepath = path+"/ratings_"+str(num_users)+"_"+str(num_items)+".csv"
+    with open(filepath, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for row in data:
+            csv_writer.writerow(row)
+    
+    # print("completed!")
 
-path = args.dir
-num_users = args.num_user
-num_items = args.num_item
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_user', help='number of users to extract', type=int, default=2000)
+    parser.add_argument('--num_item', help='number of movies to items', type=int, default=2000)
+    parser.add_argument('--dir', help='target directory for csv', default="./data")
+    args = parser.parse_args()
 
-print("path: ", path, ", num users: ", num_users, ", num items: ", num_items)
+    path = args.dir
+    num_users = args.num_user
+    num_items = args.num_item
 
-try:
-    if not os.path.exists(path):
-        os.mkdir(path)
-except OSError as er:
-    print ("Error with directory ", path, ". Error message: ", err)
-    path = "./"
-
-with open('./goodbooks-10k/ratings.csv') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    data = []  # list(csv_reader)
-    head = 0
-    for row in csv_reader:
-        # print(data)
-        if head == 0:
-            head = 1
-            continue
-
-        if int(row[0]) <= num_users and int(row[1]) <= num_items:
-            data.append(row)
-            print(row)
-
-filepath = path+"/ratings_"+str(num_users)+"_"+str(num_items)+".csv"
-with open(filepath, 'w') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    for row in data:
-        csv_writer.writerow(row)
-
-print("completed!")
+    # print("path: ", path, ", num users: ", num_users, ", num items: ", num_items)
+    create_subdata(num_users, num_items, path)
+    print("completed!")
